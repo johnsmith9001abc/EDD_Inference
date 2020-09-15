6d746b3f81eb588758925be4a54cf06e66b5ea17
Masta1337 <39281476+Masta1337@users.noreply.github.com>
2020-09-15 15:45:59 -0500

    Adding non-essential feature
    Added a secret easter egg 8)

--- app/main.py
+++ app/main.py
@@ -11,3 +11,6 @@ def read_root():
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: str = None):
     return {"item_id": item_id, "q": q}
+
+@app.get("/secret-site")
+    return {"Achievement Unlocked"}
